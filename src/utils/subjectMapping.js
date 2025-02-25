f65a0f2adf454f206996f658b6b184da8ab1da29
Kratik1093 <kratikmandloi03@gmail.com>
2025-02-25 20:20:16 +0530

    daTA

--- src/utils/subjectMapping.js
+++ src/utils/subjectMapping.js
@@ -134,5 +134,327 @@ export const subjectMapping = {
     ],
     'mca_10' : [
       { code: 'IC-1001', name: 'Thesis/Project Work' }
-    ]
+    ],
+
+    // MBA_MS Semesters
+    'mba_ms_1': [
+      { code: 'IM-203', name: 'Cost accounting' },
+      { code: 'IM-204A', name: 'Business Mathematics-II' },
+      { code: 'IM-210B', name: 'Programing using C++' },
+      { code: 'IM-214', name: 'Business Law' },
+      { code: 'IM-219', name: 'BCPD' },
+      { code: 'IM-217', name: 'Marketing Management' }
+    ],
+    'mba_ms_2': [
+      { code: 'IC-205C', name: 'Object Oriented Programming Using C++' },
+      { code: 'IC-202C', name: 'Internet and Web Programming' },
+      { code: 'IC-204B', name: 'Digital Computer Organization' },
+      { code: 'IC-201', name: 'Mathematics - II' },
+      { code: 'IC-206D', name: 'Hindi' }
+    ],
+    'mba_ms_3': [
+      { code: 'IC-311A', name: 'DBMS' },
+      { code: 'IC-305C', name: 'Data Structures' },
+      { code: 'IC-314', name: 'Probability and Statistics' },
+      { code: 'IC-315', name: 'Financial Accounting' },
+      { code: 'IC-312A', name: 'Chem & Env Sci' }
+    ],
+    'mba_ms_4': [
+      { code: 'IM-420', name: 'Purchase and Material Management' },
+      { code: 'IM-414', name: 'Financial Management - I' },
+      { code: 'IM-416A', name: 'Business Statistics - II' },
+      { code: 'IM-401C', name: 'Marketing Strategies' },
+      { code: 'IM-406B', name: 'Macro Econimics' },
+      { code: 'IM-421 ', name: 'Business Finadamentals' }
+    ],
+    'mba_ms_5': [
+      { code: 'IC-504A', name: 'System Programming' },
+      { code: 'IC-501D', name: 'Programming with Python' },
+      { code: 'IC-502C', name: 'Introduction to Data Science' },
+      { code: 'IC-503A', name: 'Computer Graphics & Multimedia' },
+      { code: 'IC-512B', name: 'Computer Oriented Numerical Methods' }
+    ],
+    'mba_ms_6': [
+      { code: 'IM-602B', name: 'Reserche Methodology' },
+      { code: 'IM-613', name: 'Business Envirinment' },
+      { code: 'IM-603A', name: 'Forcasting Techniques' },
+      { code: 'IM-601E', name: 'Operations Research' },
+      { code: 'IM-604B', name: 'Inderect Taxes' },
+      { code: 'IM-606', name: 'Project Management' }
+    ],
+    'mba_ms_7': [
+      { code: 'IC-702B', name: 'Computer Networks' },
+      { code: 'IC-701', name: 'Computer Architecture' },
+      { code: 'IC-703C', name: 'Artificial Intelligence' },
+      { code: 'IC-702A', name: 'Design & Analysis Of Algorithms' },
+      { code: 'IC-704B', name: 'Software Engineering' }
+    ],
+    'mba_ms_8': [
+      { code: 'IM-801B', name: 'Quality Management' },
+      { code: 'IM-815MA', name: 'Product And Brand Mag.' },
+      { code: 'IM-816FB', name: 'Bank Management' },
+      { code: 'IM-816MA', name: 'Strategic & Modeling in Marketing' },
+      { code: 'IM-820FB', name: 'Corporate Valuation and Restructuring' },
+      { code: 'IM-817MA', name: 'Service Marketing' },
+      { code: 'IM-821FB', name: 'Finacial Planning and Wealth Management' },
+      { code: 'IM-815FA', name: 'International Finance' },
+      { code: 'IM-818FA', name: 'Corporate Tax' },
+      { code: 'IM-816FA', name: 'Project Finance' },
+      { code: 'IM-818HA', name: 'OD' },
+      { code: 'IM-810BA', name: 'Digital Analytics' },
+      { code: 'IM-816HA', name: 'Performance and Planning Appraisal' },
+      { code: 'IM-812BA', name: 'Big Data Analysis' },
+      { code: 'IM-817HA', name: 'HA IR and Labour Laws' },
+      { code: 'IM-811BA', name: 'Predictive Modelling and pattern Discovery' },
+    ],
+    'mba_ms_9': [
+      { code: 'IC-905A', name: 'Object Oriented Analysis & Design' },
+      { code: 'IC-904A', name: 'Multimedia Systems' },
+      { code: 'IC-901A', name: 'Compiler Design' },
+      { code: 'IC-908A', name: 'Cloud Computing' },
+      { code: 'IC-909A', name: 'ITPM' }
+    ],
+    'mba_ms_10' : [
+      { code: 'IC-1001', name: 'Thesis/Project Work' }
+    ],
+
+      // MBA_ESHIP Semesters
+      'mba_es_1': [
+        { code: 'ES-100A', name: 'Entrepreneurship' },
+        { code: 'ES-105A', name: 'Principles of Management' },
+        { code: 'ES-110', name: 'Business Communication' },
+        { code: 'ES-105A', name: 'Fundamentals of Management & Marketing' },
+        { code: 'ES-107A', name: 'Creativity and Innovation' }
+      ],
+      'mba_es_2': [
+        { code: 'ES-201A', name: 'Social Enterpreneurship' },
+        { code: 'ES-202B', name: 'Business Ethics' },
+        { code: 'ES-211', name: 'Human Resource Management' },
+        { code: 'ES-205A', name: 'Service Management' },
+        { code: 'ES-208A', name: 'Emerging Trends In Business Environment' },
+        { code: 'ES-203A', name: 'Fundamantals of Finance-II' }
+      ],
+      'mba_es_3': [
+        { code: 'IC-311A', name: 'DBMS' },
+        { code: 'IC-305C', name: 'Data Structures' },
+        { code: 'IC-314', name: 'Probability and Statistics' },
+        { code: 'IC-315', name: 'Financial Accounting' },
+        { code: 'IC-312A', name: 'Chem & Env Sci' }
+      ],
+      'mba_es_4': [
+        { code: 'ES-401A', name: 'Business Legislation' },
+        { code: 'ES-404A', name: 'International Business' },
+        { code: 'ES-408A', name: 'Logistics And Supply Chain Management' },
+        { code: 'ES-410', name: 'Management Information Systems' },
+        { code: 'ES-411', name: 'Business Analytics' },
+        { code: 'ES-412 ', name: 'Business Model Development' }
+      ],
+
+
+      // MBA_APR Semesters
+      'mba_apr_1': [
+  { code: 'APR-106', name: 'Business Statistics' },
+  { code: 'APR-112A', name: 'Financial Management and Finance for Managers' },
+  { code: 'APR-112', name: 'Cost Accounting - I' },
+  { code: 'APR-102', name: 'Principles of Marketing' },
+  { code: 'APR-101B', name: 'Computer Software' },
+  { code: 'APR-216', name: 'Organizational Behavior' }
+],
+      'mba_apr_2': [
+        { code: 'APR-201', name: 'Media Planning' },
+        { code: 'APR-201A', name: 'Public Relations' },
+        { code: 'APR-202A', name: 'Advertising and PR Reserch' },
+        { code: 'APR-203', name: 'creative Marketing' },
+        { code: 'APR-206', name: 'Digital Marketing' },
+        { code: 'APR-209', name: 'Mass Communication' },
+        { code: 'APR-212', name: 'Client Servicing and Account Planning' }
+      ],
+      'mba_apr_3': [
+        { code: 'IC-311A', name: 'DBMS' },
+        { code: 'IC-305C', name: 'Data Structures' },
+        { code: 'IC-314', name: 'Probability and Statistics' },
+        { code: 'IC-315', name: 'Financial Accounting' },
+        { code: 'IC-312A', name: 'Chem & Env Sci' }
+      ],
+      'mba_apr_4': [
+        { code: 'APR-406A', name: 'BCPD' },
+        { code: 'APR-403', name: 'Direct Marketing and Event Management' },
+        { code: 'APR-401A', name: 'Rural and Retail Marketing' },
+        { code: 'APR-415', name: 'Audio Visual Production' },
+        { code: 'APR-408', name: 'Brand Management' },
+        { code: 'APR-402B', name: 'Marketing Strategies' }
+      ],
+  
+
+       // MBA(TOURISM) Semesters
+   'mba_tm_2': [
+  { code: 'TM-106', name: 'Tourism Industry' },
+  { code: 'TM-108', name: 'Language Proficiency' },
+  { code: 'TM-103', name: 'Language Proficiency (English)' },
+  { code: 'TM-104', name: 'Introduction to Tourism' },
+  { code: 'TM-105', name: 'IT for Tourism' },
+  { code: 'TM-101', name: 'Fundamentals of Mgt.' }
+],
+    'mba_tm_2': [
+      { code: 'IC-205C', name: 'Object Oriented Programming Using C++' },
+      { code: 'IC-202C', name: 'Internet and Web Programming' },
+      { code: 'IC-204B', name: 'Digital Computer Organization' },
+      { code: 'IC-201', name: 'Mathematics - II' },
+      { code: 'IC-206D', name: 'Hindi' }
+    ],
+    'mba_tm_3': [
+      { code: 'IC-311A', name: 'DBMS' },
+      { code: 'IC-305C', name: 'Data Structures' },
+      { code: 'IC-314', name: 'Probability and Statistics' },
+      { code: 'IC-315', name: 'Financial Accounting' },
+      { code: 'IC-312A', name: 'Chem & Env Sci' }
+    ],
+    'mba_tm_4': [
+      { code: 'TM-401', name: 'BCPD' },
+      { code: 'TM-402', name: 'Tour Operation-I' },
+      { code: 'TM-408', name: 'Recreatin Management' },
+      { code: 'TM-404', name: 'Tourism Product Of India' },
+      { code: 'TM-405', name: 'Legal Framework For Tourism' },
+      { code: 'TM-406', name: 'Sales and Distribution Mangement' }
+    ],
+    'mba_tm_5': [
+      { code: 'IC-504A', name: 'System Programming' },
+      { code: 'IC-501D', name: 'Programming with Python' },
+      { code: 'IC-502C', name: 'Introduction to Data Science' },
+      { code: 'IC-503A', name: 'Computer Graphics & Multimedia' },
+      { code: 'IC-512B', name: 'Computer Oriented Numerical Methods' }
+    ],
+    'mba_tm_6': [
+      { code: 'TM-601', name: 'Business Environment' },
+      { code: 'TM-602', name: 'Tourism Resources' },
+      { code: 'TM-603', name: 'Air ticketing And Fare Calculation' },
+      { code: 'TM-604', name: 'Business Statistics' },
+      { code: 'TM-605', name: 'Event Management' },
+      { code: 'TM-608', name: 'Travel Documantation' }
+    ],
+    'mba_tm_7': [
+      { code: 'IC-702B', name: 'Computer Networks' },
+      { code: 'IC-701', name: 'Computer Architecture' },
+      { code: 'IC-703C', name: 'Artificial Intelligence' },
+      { code: 'IC-702A', name: 'Design & Analysis Of Algorithms' },
+      { code: 'IC-704B', name: 'Software Engineering' }
+    ],
+    'mba_tm_8': [
+      { code: 'TM-801', name: 'Enterpreneurship' },
+      { code: 'TM-809', name: 'Fundamentals of financial Management' },
+      { code: 'TM-803', name: 'Consumer Behaviour' },
+      { code: 'TM-804', name: 'Eco Tourism' },
+      { code: 'TM-805', name: 'Rural tourism and Handi Craft' },
+      { code: 'TM-808', name: 'International Management in Tourism' }
+    ],
+    'mba_tm_9': [
+      { code: 'IC-905A', name: 'Object Oriented Analysis & Design' },
+      { code: 'IC-904A', name: 'Multimedia Systems' },
+      { code: 'IC-901A', name: 'Compiler Design' },
+      { code: 'IC-908A', name: 'Cloud Computing' },
+      { code: 'IC-909A', name: 'ITPM' }
+    ],
+    'mba_tm_10' : [
+      { code: 'IC-1001', name: 'Thesis/Project Work' }
+    ],
+
+
+        // B COM Semesters
+        'bcom_1': [
+          { code: 'IB-101A', name: 'Principles of Managemen' },
+          { code: 'IB-103B', name: 'Financial Accounting - I' },
+          { code: 'IB-104C', name: 'HRM & Practices of Mgt' },
+          { code: 'IB-106C', name: 'Cost Accounting - I' },
+          { code: 'IB-105', name: 'Basics of Psychology' }
+        ],
+        'bcom_2': [
+          { code: 'IB-201', name: 'Financial Accounting - I' },
+          { code: 'IB-207', name: 'Management Accounting - II' },
+          { code: 'IB-203', name: 'Business Law Accounting - I' },
+          { code: 'IB-208', name: 'Human Resource Management - I' },
+          { code: 'IB-209', name: 'RDBMS' },
+          { code: 'IB-210', name: 'Business Environment '},
+          { code: 'IB-211NE', name: 'Financial Literacy '}
+        ],
+        'bcom_3': [
+          { code: 'IC-311A', name: 'DBMS' },
+          { code: 'IC-305C', name: 'Data Structures' },
+          { code: 'IC-314', name: 'Probability and Statistics' },
+          { code: 'IC-315', name: 'Financial Accounting' },
+          { code: 'IC-312A', name: 'Chem & Env Sci' }
+        ],
+        'bcom_4': [
+          { code: 'IB-401', name: 'Corporate Accounting / B-402NE Corporate Environment' },
+          { code: 'IB-403', name: 'Indirect Taxes' },
+          { code: 'IB-404', name: 'Company Law' },
+          { code: 'IB-405', name: 'Entrepreneurship' },
+          { code: 'IB-406', name: 'Accounting Information System and Package' }
+        ],
+        'bcom_5': [
+          { code: 'IC-504A', name: 'System Programming' },
+          { code: 'IC-501D', name: 'Programming with Python' },
+          { code: 'IC-502C', name: 'Introduction to Data Science' },
+          { code: 'IC-503A', name: 'Computer Graphics & Multimedia' },
+          { code: 'IC-512B', name: 'Computer Oriented Numerical Methods' }
+        ],
+        'bcom_6': [
+          { code: 'IB-601', name: 'Secretarial Practice' },
+          { code: 'IB-602', name: 'Financial Management II ' },
+          { code: 'IB-605', name: 'Public Finance and Treasury ' },
+          { code: 'IB-603NE', name: 'Auditing' },
+          { code: 'IB-606NE', name: 'Corporate Tax' }
+        ],
+        'bcom_7': [
+          { code: 'IC-702B', name: 'Computer Networks' },
+          { code: 'IC-701', name: 'Computer Architecture' },
+          { code: 'IC-703C', name: 'Artificial Intelligence' },
+          { code: 'IC-702A', name: 'Design & Analysis Of Algorithms' },
+          { code: 'IC-704B', name: 'Software Engineering' }
+        ],
+        'bcom_8': [
+          { code: 'IB-801N', name: 'Stratigic Finacial Management' },
+          { code: 'IB-802N', name: 'Enterpreneurial Finance ' },
+    
+        ],
+      
+
+         // MBAMS(2)Semesters
+         'mba_ft_1': [
+          { code: 'IC-101A', name: 'Mathematics-I' },
+          { code: 'IC-103B', name: 'Digital Electronics' },
+          { code: 'IC-104C', name: 'Problem solving using "C"' },
+          { code: 'IC-106C', name: 'English & Communicative Skills' },
+          { code: 'IC-105', name: 'PC Software' }
+        ],
+        'mba_ft_2': [
+          { code: 'FT-201', name: 'Principles of Management' },
+          { code: 'FT-202', name: 'Business Economics (Managerial Economics)' },
+          { code: 'FT-203B', name: 'Business Communication & Organizational Behavior' },
+          { code: 'FT-207A', name: 'Financial Management' },
+          { code: 'FT-215', name: 'Business Methodology' },
+          { code: 'FT-204', name: 'Business Accounting I' },
+          { code: 'FT-205A', name: 'Business Statistics and Database Management' }
+        ],
+        'mba_ft_3': [
+          { code: 'IC-311A', name: 'DBMS' },
+          { code: 'IC-305C', name: 'Data Structures' },
+          { code: 'IC-314', name: 'Probability and Statistics' },
+          { code: 'IC-315', name: 'Financial Accounting' },
+          { code: 'IC-312A', name: 'Chem & Env Sci' }
+        ],
+        'mba_ft_4': [
+          { code: 'FT-405A', name: 'Production & Brand Management' },
+  { code: 'FT-406A', name: 'Banking / FT-405H Bank Management' },
+  { code: 'FT-407A', name: 'Services Marketing / FT-405H Corporate Valuation and Wealth' },
+  { code: 'FT-418', name: 'International Finance' },
+  { code: 'FT-413', name: 'Project: Management and Project Finance' },
+  { code: 'FT-415', name: 'International Financial Planning' },
+  { code: 'FT-418A ', name: 'Behavioral Management' },
+  { code: 'FT-418A ', name: 'Digital Analysis' },
+  { code: 'FT-417BA', name: 'Performance Planning & Appraisal ' },
+  { code: 'FT-415A', name: 'Advanced Pertformance Dynamics' },
+  { code: 'FT-416A', name: 'Patent, Discovery, Laws' },
+  { code: 'FT-417BA', name: 'Big Data Analysis' },
+  { code: 'FT-415A', name: 'Predictive Modelling and pattern Discovery' },
+        ],
   };
